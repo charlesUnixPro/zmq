dbc9bb22da2fcca7beab852d2257d99ef962c54c
Charles Anthony <charles.unix.pro@gmail.com>
2015-05-14 12:13:07 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -x
+
+tar xfz zeromq-4.1.0-rc1.tar.gz
+cd zeromq-4.1.0
+./configure
+make
+make check
+sudo make install
+cd ..
+
+mkdir czmq-master-ff0d028b16
+cd czmq-master-ff0d028b16
+unzip ../czmq-master-ff0d028b16.zip
+rm CMakeCache.txt
+cmake . 
+make
+make test
+sudo make install
+cd ..
+
+mkdir zyre-master-9d7e62d2c9
+cd zyre-master-9d7e62d2c9
+unzip ../zyre-master-9d7e62d2c9.zip
+rm CMakeCache.txt
+cmake .
+make
+# Hangs...
+#make test
+sudo make install
+cd ..
+
